57a62ad3fb8d5ff609d75b849ee408afc0af4fa4
tech-ticks <techticksdev@gmail.com>
2022-12-11 21:31:08 +0000

    Restore accidentally deleted Handlebars helpers (fixes automatic rank advancement dialog)

--- module/pokerole.mjs
+++ module/pokerole.mjs
@@ -65,6 +65,18 @@ Handlebars.registerHelper('concat', function() {
   return outStr;
 });
 
+// greater than
+Handlebars.registerHelper('gt', function( a, b ){
+	var next =  arguments[arguments.length-1];
+	return (a > b) ? next.fn(this) : next.inverse(this);
+});
+
+// less than
+Handlebars.registerHelper('lt', function( a, b ){
+	var next =  arguments[arguments.length-1];
+	return (a < b) ? next.fn(this) : next.inverse(this);
+});
+
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
