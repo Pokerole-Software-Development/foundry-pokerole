058d310750d0e4f83e537e8510a8b1c1a5a3711c
tech-ticks <techticksdev@gmail.com>
2023-07-02 20:13:06 +0200

    Use new API for Item Piles integration (closes #47)

--- module/helpers/integrations.mjs
+++ module/helpers/integrations.mjs
@@ -2,21 +2,18 @@
 export function registerIntegrationHooks() {
   // Item Piles module integration (https://fantasycomputer.works/FoundryVTT-ItemPiles/#/api)
   Hooks.once('item-piles-ready', async () => {
-    const promises = [
-      game.itempiles.API.setActorClassType('pokemon'),
-      game.itempiles.API.setItemQuantityAttribute('system.quantity'),
-      game.itempiles.API.setItemPriceAttribute('system.price'),
-      game.itempiles.API.setCurrencyDecimalDigits(1),
-      game.itempiles.API.setItemFilters([{
-        path: "type",
-        filters: "move,ability,effect"
-      }]),
-      game.itempiles.API.setItemSimilarities(['type', 'name'])
-    ];
-
-    // Don't overwrite existing currencies to allow customization
-    if (!game.itempiles.API.CURRENCIES?.length) {
-      promises.push(game.itempiles.API.setCurrencies([{
+    game.itempiles.API.addSystemIntegration({
+      VERSION: "1",
+      ACTOR_CLASS_TYPE: 'pokemon',
+      ITEM_QUANTITY_ATTRIBUTE: 'system.quantity',
+      ITEM_PRICE_ATTRIBUTE: 'system.price',
+      CURRENCY_DECIMAL_DIGITS: 1,
+      ITEM_FILTERS: [{
+        path: 'type',
+        filters: 'move,ability,effect'
+      }],
+      ITEM_SIMILARITIES: ['type', 'name'],
+      CURRENCIES: [{
         type: 'attribute',
         name: 'Money',
         img: 'systems/pokerole/images/icons/money.png',
@@ -26,9 +23,7 @@ export function registerIntegrationHooks() {
         data: {
           path: 'system.money'
         }
-      }]));
-    }
-
-    await Promise.all(promises);
+      }]
+    });
   });
 }
